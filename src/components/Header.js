7721ea0f987e3459ecc85091adf6dc14add173be
caseyinhaengsin <rowley.james74@gmail.com>
2020-09-28 18:06:18 -0600

    initial commit.

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+const Header = () => {
+    return (
+        <nav>
+    <div class="nav-wrapper black">
+      <a href="#" class="brand-logo right">Casey Rowley</a>
+      <ul id="nav-mobile" class="left hide-on-med-and-down">
+        <li><a href="sass.html">Work History</a></li>
+        <li><a href="badges.html">Components</a></li>
+        <li><a href="collapsible.html">JavaScript</a></li>
+      </ul>
+    </div>
+  </nav>
+    )
+}
+
+export default Header;
