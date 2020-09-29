d5e30bbe9763c29a4ce6f294534258b35f866f08
caseyinhaengsin <rowley.james74@gmail.com>
2020-09-28 18:46:02 -0600

    added sidebar that will be used to display personal info... I think.

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,15 +5,18 @@ const Header = () => {
     return (
         <div>
             <div class="ui large top fixed hidden menu">
-  <div class="ui container">
-    <a class="active item">Home</a>
-    <a class="item">Work</a>
-    <a class="item">Company</a>
-    <a class="item">Careers</a>
-    
-  </div>
-</div>
+                <div class="ui container">
+                <a class="active item">Home</a>
+                    <a class="item">Work History</a>
+                    <a class="item">Projects</a>
+                <div class="right menu">
+                    <div class="item">
+                    <a class="item">About me</a>
+                </div>
+                </div>
+            </div>
         </div>
+    </div>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './components/Header';
+import SideBar from './components/SideBar';
 
 const App = () => {
     return (
         <div>
             <Header />
+            <SideBar />
         </div>
     )
 }
